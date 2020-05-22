cf355f976ff0266e3cf7872adc6fc8f0be2fc843
jlopezc@pucp.edu.pe <Elmegacrak18>
2020-05-22 16:01:21 -0500

    Jos

---
+++ app/src/main/java/com/example/lab2_grupo2/ListaEmpleadosAdapter.java
@@ -0,0 +1,60 @@
+package com.example.lab2_grupo2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clase5.entitades.Empleado;
+
+public class ListaEmpleadosAdapter extends RecyclerView.Adapter<ListaEmpleadosAdapter.EmpleadoViewHolder> {
+Empleado [] listaEmpleados;
+Context contexto;
+public ListaEmpleadosAdapter(Empleado[] lista, Context c){
+    this.listaEmpleados=lista;
+    this.contexto=c;
+
+
+}
+public static class EmpleadoViewHolder extends RecyclerView.ViewHolder{
+    CardView cardView;
+    TextView textView10;
+    public EmpleadoViewHolder(  View itemView){
+        super(itemView);
+        cardView=itemView.findViewById(R.id.cardView1);
+        textView10=itemView.findViewById(R.id.textView10);
+
+    }
+
+
+
+}
+    @NonNull
+    @Override
+    public EmpleadoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View itemview= LayoutInflater.from(contexto).inflate(R.layout.item_rv,parent,false);
+    EmpleadoViewHolder empleadoViewHolder=new EmpleadoViewHolder(itemview);
+
+
+        return empleadoViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder( EmpleadoViewHolder holder, int position) {
+    Empleado e =listaEmpleados[position];
+    String text=e.getFirst_name()+"" + e.getLast_name();
+    holder.textView10.setText(text);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaEmpleados.length;
+    }
+}
---
+++ app/src/main/java/com/example/lab2_grupo2/entitades/DtoEmpleado.java
@@ -0,0 +1,23 @@
+package com.example.lab2_grupo2.entitades;
+
+public class DtoEmpleado {
+
+    private String estado;
+    private Empleado[] lista;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Empleado[] getLista() {
+        return lista;
+    }
+
+    public void setLista(Empleado[] lista) {
+        this.lista = lista;
+    }
+}
