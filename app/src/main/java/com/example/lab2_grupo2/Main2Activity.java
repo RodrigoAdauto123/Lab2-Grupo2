4f69458b7ce0f08048eca82c3845c4249e607df4
jlopezc@pucp.edu.pe <Elmegacrak18>
2020-05-23 08:41:38 -0500

    Last

--- app/src/main/java/com/example/lab2_grupo2/Main2Activity.java
+++ app/src/main/java/com/example/lab2_grupo2/Main2Activity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,9 +36,46 @@ public class Main2Activity extends AppCompatActivity {
 
 
     }
-    public void obtenerDeInternet(View view) {
+    public boolean isInternetAvailable() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        String url = "http://ec2-54-165-73-192.compute-1.amazonaws.com:9000";
+        if (connectivityManager == null)
+            return false;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Network networks = connectivityManager.getActiveNetwork();
+            if (networks == null)
+                return false;
+
+            NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(networks);
+            if (networkCapabilities == null)
+                return false;
+
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI))
+                return true;
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+                return true;
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
+                return true;
+            return false;
+        } else {
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            if (activeNetworkInfo == null)
+                return false;
+            if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI)
+                return true;
+            if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+                return true;
+            if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_ETHERNET)
+                return true;
+            return false;
+
+        }
+    }
+    public void obtenerDeInternetApiKEy(View view) {
+
+        String url = "http://ec2-54-165-73-192.compute-1.amazonaws.com:9000/getApiKey";
         StringRequest stringRequest = new StringRequest(StringRequest.Method.GET, url,
 
                 new Response.Listener<String>() {
@@ -56,8 +99,10 @@ public class Main2Activity extends AppCompatActivity {
                 }) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
+                View view1=findViewById(R.id.ObtenerAPIKEYButton);
+                String groupKey=view1.toString();
                 Map<String, String> cabeceras = new HashMap<>();
-                cabeceras.put("api-key", "EaQibIyUgcoCAyelLnDwUAxR1OX6AH");
+                cabeceras.put("api-key", groupKey);
                 return cabeceras;
             }
         };
