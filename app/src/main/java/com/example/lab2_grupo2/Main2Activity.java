5d12e96562343f4275ce7067603b899b49f58301
jlopezc@pucp.edu.pe <Elmegacrak18>
2020-05-22 22:31:29 -0500

    Last

--- app/src/main/java/com/example/lab2_grupo2/Main2Activity.java
+++ app/src/main/java/com/example/lab2_grupo2/Main2Activity.java
@@ -5,22 +5,35 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.example.lab2_grupo2.entitades.DtoEmpleado;
 import com.example.lab2_grupo2.entitades.Empleado;
 import com.google.gson.Gson;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main2Activity extends AppCompatActivity {
 
     static String apiKey;
 
-    public void obtenerDeInternet(View view){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
 
-        String url="http://ec2-54-165-73-192.compute-1.amazonaws.com:9000";
-        StringRequest stringRequest=new StringRequest(StringRequest.Method.GET, url,
+
+    }
+    public void obtenerDeInternet(View view) {
+
+        String url = "http://ec2-54-165-73-192.compute-1.amazonaws.com:9000";
+        StringRequest stringRequest = new StringRequest(StringRequest.Method.GET, url,
 
                 new Response.Listener<String>() {
 
@@ -33,17 +46,22 @@ public class Main2Activity extends AppCompatActivity {
                         RecyclerView recyclerView = findViewById(R.id.recyclerView);
                         recyclerView.setAdapter(listaEmpleadosAdapter);
                         recyclerView.setLayoutManager(new LinearLayoutManager(Main2Activity.this));
-                    }}
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("errorVol", error.getMessage());
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> cabeceras = new HashMap<>();
+                cabeceras.put("api-key", "EaQibIyUgcoCAyelLnDwUAxR1OX6AH");
+                return cabeceras;
+            }
+        };
 
-    }
+    } }
 
 
-
-                        @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-
-    }
-}
